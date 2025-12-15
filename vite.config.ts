1e326b6d67f8c8259409b4ee0c1b400572be781a
荣顶 <wangrongding@qq.com>
2025-12-16 02:48:39 +0800

    chore: 更新构建命令和配置，优化 Tauri 开发环境

--- vite.config.ts
+++ vite.config.ts
@@ -1,32 +1,42 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
-// @ts-expect-error process is a nodejs global
-const host = process.env.TAURI_DEV_HOST;
-
+const host = process.env.TAURI_DEV_HOST
 // https://vite.dev/config/
 export default defineConfig(async () => ({
   plugins: [react()],
 
   // Vite options tailored for Tauri development and only applied in `tauri dev` or `tauri build`
   //
-  // 1. prevent Vite from obscuring rust errors
+  // 防止 Vite 清除 Rust 显示的错误
   clearScreen: false,
   // 2. tauri expects a fixed port, fail if that port is not available
   server: {
     port: 1420,
+    // Tauri 工作于固定端口，如果端口不可用则报错
     strictPort: true,
+    // 如果设置了 host，Tauri 则会使用
     host: host || false,
     hmr: host
       ? {
-          protocol: "ws",
+          protocol: 'ws',
           host,
           port: 1421,
         }
       : undefined,
     watch: {
       // 3. tell Vite to ignore watching `src-tauri`
-      ignored: ["**/src-tauri/**"],
+      ignored: ['**/src-tauri/**'],
     },
   },
-}));
+  // 添加有关当前构建目标的额外前缀，使这些 CLI 设置的 Tauri 环境变量可以在客户端代码中访问
+  envPrefix: ['VITE_', 'TAURI_ENV_*'],
+  build: {
+    // Tauri 在 Windows 上使用 Chromium，在 macOS 和 Linux 上使用 WebKit
+    target: process.env.TAURI_ENV_PLATFORM == 'windows' ? 'chrome105' : 'safari13',
+    // 在 debug 构建中不使用 minify
+    // minify: !process.env.TAURI_ENV_DEBUG ? 'esbuild' : false, // minify?: boolean | "terser" | "esbuild";
+    // 在 debug 构建中生成 sourcemap
+    sourcemap: !!process.env.TAURI_ENV_DEBUG,
+  },
+}))
