1e4a3d75e00ad8cab92a47dbce58f529e2911119
荣顶 <wangrongding@qq.com>
2025-12-29 01:38:12 +0800

    chore: 添加 Prettier 配置文件以支持代码格式化

---
+++ prettier.config.js
@@ -0,0 +1,7 @@
+export default {
+  semi: true, // 去掉行尾分号
+  singleQuote: false, // 使用单引号
+  tabWidth: 2, // 缩进宽度为2个空格
+  trailingComma: "es5", // 尾随逗号仅在ES5有效的地方使用
+  arrowParens: "always", // 箭头函数参数总是使用括号
+};
