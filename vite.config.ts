a9a9cb091bc6eae70578dcc7f7b50ebedb506c5e
荣顶 <wangrongding@qq.com>
2026-07-04 09:46:05 +0800

    feat: add @ai-ins/vite plugin to Vite configuration

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,11 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
+import aiIns from "@ai-ins/vite";
 const host = process.env.TAURI_DEV_HOST;
 // https://vite.dev/config/
 export default defineConfig(async () => ({
-  plugins: [react()],
+  plugins: [aiIns(), react()],
 
   // Vite options tailored for Tauri development and only applied in `tauri dev` or `tauri build`
   //
